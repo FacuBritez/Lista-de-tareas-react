d20a1858a2a745bccee274a4d19c73aff80eda0b
FacuBritez <FacundoIvanBritez@gmail.com>
2023-06-18 08:24:43 -0300

    Primer commit Lista-de-tareas-react

---
+++ src/components/Date.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../stylesheets/Date.sass'
+
+function Date(){
+    return (
+        <>
+            <div className="dateRight">
+
+            </div>
+            <div className="dateText">
+
+            </div>
+        </>
+    );
+}
+
+export default Date;
---
+++ src/components/Form.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../stylesheets/Form.sass'
+
+function Form() {
+    return (
+        <form>
+            <input type="text" />
+            <button>+</button>
+            <button>Ordenar</button>
+            <button>trash</button>
+        </form>
+    );
+}
+
+export default Form;
