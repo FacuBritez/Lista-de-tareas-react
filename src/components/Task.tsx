ef6692bde3c9ac4b79801e2389553d6db00c7d04
FacuBritez <FacundoIvanBritez@gmail.com>
2023-06-19 12:17:37 -0300

    Base de Task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import '../stylesheets/Task.sass'
 
-function Task() {
+function Task(props: any) {
+
+    const removeTask = (id: any) => {
+        console.log('removeTask')
+        /*Borrar elemento con id unico uuidv4*/
+    }
+
+    const tachar = (id: any) => {
+        /*
+       const task = la tare;
+        task.classList.toggle('done');
+        */
+    }
+
     return (
-        <>
-        
-        </>
-    );
-}
+        //Se crea la nueva tarea
+        <div className="task" onClick={() => tachar(props.id)}>
+            {props.text}
+            <i className="trash fa-regular fa-trash-can" onClick={() => removeTask(props.id)}>
+                icon
+            </i>
+        </div>
+    )
+};
 
 export default Task;
