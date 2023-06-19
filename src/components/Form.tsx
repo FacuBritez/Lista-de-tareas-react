2ccc3100348c3750b3445e42e2fdde2045e0c51f
FacuBritez <FacundoIvanBritez@gmail.com>
2023-06-19 12:16:12 -0300

    tasksContainer añadido a form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,26 +7,31 @@ function Form() {
     const addNewTask = () => {
         console.log('addNewTask')
     }
+
     const order = () => {
         console.log('order')
     }
+
     const trash = () => {
         console.log('trash')
     }
- 
+
     return (
-        <form>
-            <input
-                type="text"
-                id="input"
-                placeholder="Nueva tarea"
-                autoComplete="off"
+        <>
+            <form>
+                <input
+                    type="text"
+                    id="input"
+                    placeholder="Nueva tarea"
+                    autoComplete="off"
                 //Añadir keyup enter
-            />
-            <Button type='button' text='+' className='addTaskButton' onClick={() => addNewTask()}/>
-            <Button type='button' text='Ordenar' className='orderButton' onClick={() => order()}/>
-            <Button type='button' text='trash' className='removeButton' onClick={() => trash()}/>
-        </form>
+                />
+                <Button type='button' text='+' className='addTaskButton' onClick={() => addNewTask()} />
+                <Button type='button' text='Ordenar' className='orderButton' onClick={() => order()} />
+                <Button type='button' text='trash' className='removeButton' onClick={() => trash()} />
+            </form>
+            <div id="tasksContainer" />
+        </>
     );
 }
 
